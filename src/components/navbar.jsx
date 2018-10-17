acbd22780ed0d2e58f9ae35b2c14642f25452f01
shitashikat <radistkakat89@gmail.com>
2018-10-17 15:21:21 +0300

    react counter-app

---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+//Stateless Functional Component - sfc
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+/*
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+*/
+
+export default NavBar;
